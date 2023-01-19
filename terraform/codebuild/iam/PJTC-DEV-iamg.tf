12af8f11cdd9c224425d780b7dfdfccb069ecfd0
nathanielbaek <45681816+nathanielbaek@users.noreply.github.com>
2023-01-19 14:47:37 +0900

    iam

--- terraform/codebuild/iam/PJTC-DEV-iamg.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_iam_group" "Admin-Group" {
-    name = "Admin-Group"
-    path = "/"
-}
-
--- terraform/codebuild/iam/PJTC-DEV-iamgm.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_iam_group_membership" "Admin-Group" {
-    name  = "Admin-Group-group-membership"
-    users = ["lhk8710", "dev-pub1", "seonok", "hyjung0801", "ohbaek", "anguklee"]
-    group = "Admin-Group"
-}
-
